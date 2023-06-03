876642614cb3d4ca2e3f97de00485dea78241632
zillurahman10 <zillur.hero40@gmail.com>
2023-06-03 13:15:18 +0600

    [Done] get reviews

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 
 // Middleware
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-const uri = process.env.PORT;
+const uri = "mongodb+srv://zillurhero40:wUFi7nB9M8P2mGeb@cluster0.vwebhjn.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,12 +26,32 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+        console.log('database is connected succeessfully 🔥');
+        const coffeesCollection = client.db("Coffee-Buddy").collection("coffees")
+        const reviewsCollection = client.db("Coffee-Buddy").collection("reviews")
+
+        // Api
+        // get all coffees
+        app.get('/coffees', async (req, res) => {
+            const query = {}
+            const cursor = coffeesCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // get all reviews
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
     } catch (err) {
         console.log(err);
     }
 }
-run().catch(console.dir);
+run()
 
-
-// zillurhero40
-// wUFi7nB9M8P2mGeb
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
